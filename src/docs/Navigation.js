2ff3d8c0bf45892a86744b8d7782ba63c75b842b
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2018-07-24 11:03:27 -0500

    doc

---
+++ src/docs/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navigation = ({components}) => {
+    return (
+        <ul className="navigation">
+            {
+                components.map( name => {
+                    return (
+                        <li key={name}>
+                            <a href={`#${name}`}>{name}</a>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
