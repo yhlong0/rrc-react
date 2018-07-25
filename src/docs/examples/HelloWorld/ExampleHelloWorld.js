5bfe8c3883e083d596daed90d0851ea14c9446d8
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2018-07-25 17:13:09 -0500

    change path

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld/HelloWorld';
+
+/** Custom Message */
+export default function ExampleHelloWorld() {
+    return <HelloWorld message="First component!" />
+}
