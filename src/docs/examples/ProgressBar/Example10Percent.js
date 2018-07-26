b4f6a7cab09913436cc48830b6b168970bcfacff
yhlong0 <yhlong2@gmail.com>
2018-07-25 22:24:24 -0500

    finish progress bar

---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+/** Custom Message */
+export default function Example10Percent() {
+    return <ProgressBar height={7} width={200} percent={100}  />
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
 import './index.css';
-import '../node_modules/highlight.js/styles/vs.css';
+import '../node_modules/highlight.js/styles/ocean.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Docs />, document.getElementById('root'));
